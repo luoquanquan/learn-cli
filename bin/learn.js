eadf6d39b6d1498cdf328ba45e0bf10422684d83
luoquanquan <luoquanquan@baidu.com>
2018-11-13 21:24:45 +0800

    配置 eslint 保证代码质量

--- bin/learn.js
+++ bin/learn.js
@@ -1,42 +1,43 @@
 #!/usr/bin/env node
-const program = require('commander')
-program
-.version('0.0.1')
-.option('-C, --chdir <path>', 'change the working directory')
-.option('-c, --config <path>', 'set config path. defaults to ./deploy.conf')
-.option('-T, --no-tests', 'ignore test hook')
+const program = require('commander');
 
 program
-.command('setup')
-.description('run remote setup commands')
-.action(function() {
-  console.log('setup');
-});
+  .version('0.0.1')
+  .option('-C, --chdir <path>', 'change the working directory')
+  .option('-c, --config <path>', 'set config path. defaults to ./deploy.conf')
+  .option('-T, --no-tests', 'ignore test hook');
 
 program
-.command('exec <cmd>')
-.description('run the given remote command')
-.action(function(cmd) {
-  console.log('exec "%s"', cmd);
-});
+  .command('setup')
+  .description('run remote setup commands')
+  .action(() => {
+    console.log('setup');
+  });
 
 program
-.command('teardown <dir> [otherDirs...]')
-.description('run teardown commands')
-.action(function(dir, otherDirs) {
-  console.log('dir "%s"', dir);
-  if (otherDirs) {
-    otherDirs.forEach(function (oDir) {
-      console.log('dir "%s"', oDir);
-    });
-  }
-});
+  .command('exec <cmd>')
+  .description('run the given remote command')
+  .action((cmd) => {
+    console.log('exec "%s"', cmd);
+  });
 
 program
-.command('*')
-.description('deploy the given env')
-.action(function(env) {
-  console.log('deploying "%s"', env);
-});
+  .command('teardown <dir> [otherDirs...]')
+  .description('run teardown commands')
+  .action((dir, otherDirs) => {
+    console.log('dir "%s"', dir);
+    if (otherDirs) {
+      otherDirs.forEach((oDir) => {
+        console.log('dir "%s"', oDir);
+      });
+    }
+  });
+
+program
+  .command('*')
+  .description('deploy the given env')
+  .action((env) => {
+    console.log('deploying "%s"', env);
+  });
 
 program.parse(process.argv);
