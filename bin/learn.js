43a43babf405b47591869cea380226ba34fd56eb
luoquanquan <luoquanquan@baidu.com>
2018-11-13 21:22:51 +0800

    通过 commander 接收命令行传来的参数

--- bin/learn.js
+++ bin/learn.js
@@ -1,2 +1,42 @@
 #!/usr/bin/env node
-console.log(process.argv[2] + '好好学习, 天天向上')
+const program = require('commander')
+program
+.version('0.0.1')
+.option('-C, --chdir <path>', 'change the working directory')
+.option('-c, --config <path>', 'set config path. defaults to ./deploy.conf')
+.option('-T, --no-tests', 'ignore test hook')
+
+program
+.command('setup')
+.description('run remote setup commands')
+.action(function() {
+  console.log('setup');
+});
+
+program
+.command('exec <cmd>')
+.description('run the given remote command')
+.action(function(cmd) {
+  console.log('exec "%s"', cmd);
+});
+
+program
+.command('teardown <dir> [otherDirs...]')
+.description('run teardown commands')
+.action(function(dir, otherDirs) {
+  console.log('dir "%s"', dir);
+  if (otherDirs) {
+    otherDirs.forEach(function (oDir) {
+      console.log('dir "%s"', oDir);
+    });
+  }
+});
+
+program
+.command('*')
+.description('deploy the given env')
+.action(function(env) {
+  console.log('deploying "%s"', env);
+});
+
+program.parse(process.argv);
